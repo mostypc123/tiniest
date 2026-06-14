dcde664c5720e44e626e3b76ad3b7ebb72a1eb15
mostypc123 <mostypc123@redroselinux.org>
2026-06-14 21:21:07 +0200

    feat: add mkdir command

---
+++ src/mkdir.cpp
@@ -0,0 +1,34 @@
+#include <sys/stat.h>
+#include <iostream>
+#include <cstring>
+
+/*
+ * The `mkdir` command creates dirs.
+ * insert big brain meme here
+ *
+ * The tiniest implementation of mkdir has the -p flag.
+ */
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "mkdir: need to specify dir to create" << std::endl;
+        return 2;
+    }
+
+    bool exist_ok = false;
+    std::string dir = argv[1];
+    if (!strcmp(argv[1], "-p")) {
+        if (argc == 2) {
+            std::cout << "mkdir: need to specify dir to create" << std::endl;
+            return 2;
+        }
+        exist_ok = true;
+        dir = argv[2];
+    }
+
+    if (exist_ok) {
+        mkdir(dir.c_str(), 0755);
+    } else {
+        return mkdir(dir.c_str(), 0755);
+    }
+}
