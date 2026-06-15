907af42e67a2d0bc98a8d67d10b1234259222ea1
mostypc123 <mostypc123@redroselinux.org>
2026-06-15 15:13:24 +0200

    improve touch

--- src/touch.cpp
+++ src/touch.cpp
@@ -1,18 +1,39 @@
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
 
 /*
  * The `touch` command creates a new file.
  */
+namespace tiniest {
+    static inline int touch(std::string file_) {
+        if (access(file_.c_str(), F_OK) != 0) {
+            std::cout << "touch: file already exists" << std::endl;
+            return 1;
+        }
+
+        std::ofstream file(file_);
+        if (!file.is_open()) {
+            std::cout << "touch: failed to open " << file_ << std::endl;
+            return 1;
+        }
+        file << "";
+        return 0;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
         std::cout << "touch: you must specify the file to create." << std::endl;
         return 2;
     }
-    std::string file_arg = argv[1];
 
-    std::ofstream file(file_arg);
-    file << "";
+    for (int i = 1; i < argc; i++) {
+        if (tiniest::touch(argv[i]) == 1) {
+            std::cout << "touch: failed to create " << argv[i] << std::endl;
+            return 1;
+        }
+    }
+
     return 0;
 }
