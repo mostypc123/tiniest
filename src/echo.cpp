4de4d6d8cd8c4c6e55eb405d9cceab2fd0bf3297
/home/neo <158327205+neoapps-dev@users.noreply.github.com>
2026-07-07 14:48:04 +0300

    put space between echoed output args smh (i didnt wanna merge this)

--- src/echo.cpp
+++ src/echo.cpp
@@ -13,7 +13,7 @@
 
 int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
-        std::cout << argv[i];
+        std::cout << argv[i] << " ";
     }
     std::cout << std::endl;
 
